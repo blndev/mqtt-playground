93b4abe5e2d492c7630972ac0e0e3aaa1d4b8307
blndev <blndev@users.noreply.github.com>
2019-08-07 19:08:48 +0200

    basic setup

---
+++ iot-device-01/main.py
@@ -0,0 +1,17 @@
+import paho.mqtt.client as mqtt
+
+# Client(client_id="", clean_session=True, userdata=None, protocol=MQTTv311, transport="tcp")
+
+# client =mqtt.Client(client_name)
+# #connect(host, port=1883, keepalive=60, bind_address="")
+# client.connect(host_name)
+
+# publish(topic, payload=None, qos=0, retain=False)
+
+
+import paho.mqtt.client as mqtt #import the client1
+broker_address="127.0.0.1" 
+#broker_address="iot.eclipse.org" #use external broker
+client = mqtt.Client("P1") #create new instance
+client.connect(broker_address) #connect to broker
+client.publish("house/main-light","OFF")#publish
