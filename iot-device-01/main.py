8a23b579daba6728604f3e34ff1b8412994f80a2
blndev <blndev@users.noreply.github.com>
2019-08-07 22:55:50 +0200

    add receiver

--- iot-device-01/main.py
+++ iot-device-01/main.py
@@ -13,5 +13,12 @@ import paho.mqtt.client as mqtt #import the client1
 broker_address="127.0.0.1" 
 #broker_address="iot.eclipse.org" #use external broker
 client = mqtt.Client("P1") #create new instance
+print("connecting to broker")
 client.connect(broker_address) #connect to broker
 client.publish("house/main-light","OFF")#publish
+
+
+print("Subscribing to topic","house/bulbs/bulb1")
+client.subscribe("house/bulbs/bulb1")
+print("Publishing message to topic","house/bulbs/bulb1")
+client.publish("house/bulbs/bulb1","OFF")
