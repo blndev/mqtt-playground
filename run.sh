0e47f5a8b97e3731950f27afe2de4a54d6a448f7
blndev <blndev@users.noreply.github.com>
2019-09-17 15:00:22 +0200

    add nodered

--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
-docker run -it -p 1883:1883 -p 9001:9001 -v mosquitto.conf:/mosquitto/mosquitto.conf eclipse-mosquitto
+docker run -d \
+    -p 1883:1883 \
+    -p 9001:9001 \
+    -v mosquitto.conf:/mosquitto/mosquitto.conf \
+    --name mosquitto \
+    eclipse-mosquitto
+
+
+docker run    \
+    -d           \
+    -p 1880:1880  \
+    --name nodered \
+    --link mosquitto  \
+    nodered/node-red-docker
