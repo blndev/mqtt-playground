2d909bcb18baf5329ef67e4c5b7cc6f30a6e2964
blndev <blndev@users.noreply.github.com>
2019-09-17 18:51:13 +0200

    playground with two message brokers

--- iot-device-01/main.py
+++ iot-device-01/main.py
@@ -1,26 +1,36 @@
 import paho.mqtt.client as mqtt
+import time
 
-# Client(client_id="", clean_session=True, userdata=None, protocol=MQTTv311, transport="tcp")
+# clientMOQ(clientMOQ_id="", clean_session=True, userdata=None, protocol=MQTTv311, transport="tcp")
 
-# client =mqtt.Client(client_name)
+# clientMOQ =mqtt.clientMOQ(clientMOQ_name)
 # #connect(host, port=1883, keepalive=60, bind_address="")
-# client.connect(host_name)
+# clientMOQ.connect(host_name)
 
 # publish(topic, payload=None, qos=0, retain=False)
 
 
-import paho.mqtt.client as mqtt #import the client1
+import paho.mqtt.client as mqtt #import the clientMOQ1
 broker_address="127.0.0.1" 
 #broker_address="iot.eclipse.org" #use external broker
-client = mqtt.Client("P1") #create new instance
+clientMOQ = mqtt.Client("iot-1-c1") #create new instance
+clientHMQ = mqtt.Client("iot-1-c2") #create new instance
+
 print("connecting to broker")
-client.connect(broker_address) #connect to broker
-client.publish("house/main-light","OFF")#publish
+clientMOQ.connect(broker_address) #connect to broker
+clientHMQ.connect(broker_address, 1884) #connect to broker
+
+clientMOQ.publish("house/main-light","OFF")#publish
 
 
-print("Subscribing to topic","house/bulbs/bulb1")
 #no need to subscribe when you just want to send messages
-#client.subscribe("house/bulbs/bulb1")
+#print("Subscribing to topic","house/bulbs/bulb1")
+#clientMOQ.subscribe("house/bulbs/bulb1")
 print("Publishing message to topic","house/bulbs/bulb1")
-client.publish("house/bulbs/bulb1","OFF")
-client.publish("house/bulbs/bulb2","ON")
+clientMOQ.publish("house/bulbs/bulb1","OFF")
+print("Publishing message to topic","house/bulbs/bulb2")
+clientMOQ.publish("house/bulbs/bulb2","ON")
+print("Publishing message to topic","house/bulbs/bulb3")
+clientHMQ.publish("house/bulbs/bulb3","ON")
+
+time.sleep(60) # wait
