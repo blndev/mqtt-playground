b0bc29fde7bf0a180b0d14d00746a395159681c0
blndev <blndev@users.noreply.github.com>
2019-09-18 19:29:29 +0200

    activate docker compose

--- run.sh
+++ run.sh
@@ -1,14 +1,4 @@
-docker run -d \
-    -p 1883:1883 \
-    -p 9001:9001 \
-    -v mosquitto.conf:/mosquitto/mosquitto.conf \
-    --name mosquitto \
-    eclipse-mosquitto
 
-
-docker run    \
-    -d           \
-    -p 1880:1880  \
-    --name nodered \
-    --link mosquitto  \
-    nodered/node-red-docker
+pip install --user -r requirements.txt
+docker compose up -d
+python3 ./iot-device-01/main.py
